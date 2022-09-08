4cd3840d682ad6117880566fdf43689a11408a0c
Ethan Carter <ethancarter@Ethans-MBP-77.app.byu.edu>
2022-09-08 13:48:34 -0600

    reorderd the data_setup dataframs to only account for relevant rows

--- data_setup.py
+++ data_setup.py
@@ -6,15 +6,14 @@ df = pd.read_csv('FullDataset2022-07-28.csv')
 #4 potential targets:
 #Suicide Considered, Suicide Desire, Suicide Intention, Suicide Resist, Suicide Last Half Hour
 #All suicide questions should be removed from the table before training
+#Person identifiers should also be removed
 
-evening_survey = df.loc[:, ('IDKey', 'DateKey', 'UserID_E', 'ResponseID_E', 'StartDate_E', 'StartTime_E', 'SubmitTime_E',
- 'TimeZone_E', 'TriggerID_E', 'TriggerDate_E', 'TriggerTime_E', 'OS_E', 'Location_E', 'EnergyLevels', 'Motivation',
+evening_survey = df.loc[:, ('Location_E', 'EnergyLevels', 'Motivation',
   'Productivity', 'FeltIll', 'FoodHealthiness', 'PhysicalActivity', 'LeisurePhysicalMinutes', 'NegFeelings',
    'ReduceNegFeelings', 'PosFeelings', 'IncreasePosFeelings', 'EnjoyTheMoment', 'NotShowFeelings', 'AcceptedFeeling',
     'SelfFault', 'ChangedThoughts', 'RemindFeelingsDontLast', 'StressedLog', 'StressedScore', 'StressManage', 'StressInterfere',
      'PeopleInteractionFace', 'PeopleInteractionDigital', 'ConnectFaceScore', 'ConnectDigitalScore', 'DesireSpendTime',
-      'HowSupportedFelt', 'SpiritualConnection', 'NumNaps', 'ElapsedNapTotal', 'StartDatetime_E', 'ElapsedSurveySecs_E',
-       'MetricwireID', 'FinishedLog', 'Group', 'VisitsCompleted', 'Age', 'Sex', 'IQ', 'Race')]
+      'HowSupportedFelt', 'SpiritualConnection', 'NumNaps', 'ElapsedNapTotal', 'ElapsedSurveySecs_E', 'Age', 'Sex', 'IQ', 'Race')]
 
 target_considered_E = df.loc[:, ('SuicideConsidered_E')]
 target_inteneded_E = df.loc[:, 'SuicideIntention_E']
@@ -23,10 +22,8 @@ target_resist_E = df.loc[:, 'SuicideResist_E']
 target_halfhour_E = df.loc[:, 'SuicideLastHalfHour_E']
 target_composite_E = df.loc[:, ('SuicideComposite_E')]
 
-morning_survey = df.loc[:, ('IDKey', 'DateKey', 'UserID_M', 'ResponseID_M', 'StartDate_M', 'StartTime_M', 'SubmitTime_M',
- 'TimeZone_M', 'TriggerID_M', 'TriggerDate_M', 'TriggerTime_M', 'OS_M', 'Location_M', 'TimeToBed', 'ElapsedSleepWait',
-  'NumWokeUp', 'ElapsedWokeUp', 'NightmareLog', 'NightmareScore', 'TimeAwake', 'ElapsedSleepTotal', 'SleepQuality',
-   'StartDatetime_M', 'ElapsedSurveySecs_M', 'MetricwireID', 'FinishedLog', 'Group', 'VisitsCompleted', 'Age', 'Sex', 'IQ', 'Race')]
+morning_survey = df.loc[:, ('TimeToBed', 'ElapsedSleepWait', 'NumWokeUp', 'ElapsedWokeUp', 'NightmareLog', 'NightmareScore',
+ 'TimeAwake', 'ElapsedSleepTotal', 'SleepQuality', 'Age', 'Sex', 'IQ', 'Race')]
 
 target_considered_M = df.loc[:, ('SuicideConsidered_M')]
 target_inteneded_M = df.loc[:, 'SuicideIntention_M']
@@ -35,8 +32,8 @@ target_resist_M = df.loc[:, 'SuicideResist_M']
 target_halfhour_M = df.loc[:, 'SuicideLastHalfHour_M']
 target_composite_M = df.loc[:, ('SuicideComposite_M')]
 
-phone_data = df.loc[:, ('IDKey', 'DateKey', 'Date', 'Creativity', 'Education', 'Entertainment', 'Information_Reading',
- 'Productivity_Finance', 'Social', 'Travel', 'Utilities', 'Shopping_Food', 'Other', 'Games', 'Health and Fitness', 'OS')]
+phone_data = df.loc[:, ('Creativity', 'Education', 'Entertainment', 'Information_Reading', 'Productivity_Finance', 'Social',
+ 'Travel', 'Utilities', 'Shopping_Food', 'Other', 'Games', 'Health and Fitness')]
 
 le = preprocessing.LabelEncoder()
 #each column may need its own label encoder?
