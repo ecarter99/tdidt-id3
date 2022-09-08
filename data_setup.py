22eeb43f5ad100112be9673ad448ba539013f267
Ethan Carter <ethancarter@Ethans-MBP-77.app.byu.edu>
2022-09-08 13:59:17 -0600

    retry commit datasetup

--- data_setup.py
+++ data_setup.py
@@ -1,40 +1,44 @@
 import pandas as pd
-from sklearn import preprocessing
-
-df = pd.read_csv('FullDataset2022-07-28.csv')
 
 #4 potential targets:
 #Suicide Considered, Suicide Desire, Suicide Intention, Suicide Resist, Suicide Last Half Hour
 #All suicide questions should be removed from the table before training
 #Person identifiers should also be removed
 
-evening_survey = df.loc[:, ('Location_E', 'EnergyLevels', 'Motivation',
+def get_evening(df):
+   evening_survey = df.loc[:, ('Location_E', 'EnergyLevels', 'Motivation',
   'Productivity', 'FeltIll', 'FoodHealthiness', 'PhysicalActivity', 'LeisurePhysicalMinutes', 'NegFeelings',
    'ReduceNegFeelings', 'PosFeelings', 'IncreasePosFeelings', 'EnjoyTheMoment', 'NotShowFeelings', 'AcceptedFeeling',
     'SelfFault', 'ChangedThoughts', 'RemindFeelingsDontLast', 'StressedLog', 'StressedScore', 'StressManage', 'StressInterfere',
      'PeopleInteractionFace', 'PeopleInteractionDigital', 'ConnectFaceScore', 'ConnectDigitalScore', 'DesireSpendTime',
       'HowSupportedFelt', 'SpiritualConnection', 'NumNaps', 'ElapsedNapTotal', 'ElapsedSurveySecs_E', 'Age', 'Sex', 'IQ', 'Race')]
 
-target_considered_E = df.loc[:, ('SuicideConsidered_E')]
-target_inteneded_E = df.loc[:, 'SuicideIntention_E']
-target_desired_E = df.loc[:, 'SuicideDesire_E']
-target_resist_E = df.loc[:, 'SuicideResist_E']
-target_halfhour_E = df.loc[:, 'SuicideLastHalfHour_E']
-target_composite_E = df.loc[:, ('SuicideComposite_E')]
+   return evening_survey
 
-morning_survey = df.loc[:, ('TimeToBed', 'ElapsedSleepWait', 'NumWokeUp', 'ElapsedWokeUp', 'NightmareLog', 'NightmareScore',
- 'TimeAwake', 'ElapsedSleepTotal', 'SleepQuality', 'Age', 'Sex', 'IQ', 'Race')]
+def get_target(df, column):
+   """
+   target_considered_E = df.loc[:, ('SuicideConsidered_E')]
+   target_inteneded_E = df.loc[:, 'SuicideIntention_E']
+   target_desired_E = df.loc[:, 'SuicideDesire_E']
+   target_resist_E = df.loc[:, 'SuicideResist_E']
+   target_halfhour_E = df.loc[:, 'SuicideLastHalfHour_E']
+   target_composite_E = df.loc[:, ('SuicideComposite_E')]
 
-target_considered_M = df.loc[:, ('SuicideConsidered_M')]
-target_inteneded_M = df.loc[:, 'SuicideIntention_M']
-target_desired_M = df.loc[:, 'SuicideDesire_M']
-target_resist_M = df.loc[:, 'SuicideResist_M']
-target_halfhour_M = df.loc[:, 'SuicideLastHalfHour_M']
-target_composite_M = df.loc[:, ('SuicideComposite_M')]
+   target_considered_M = df.loc[:, ('SuicideConsidered_M')]
+   target_inteneded_M = df.loc[:, 'SuicideIntention_M']
+   target_desired_M = df.loc[:, 'SuicideDesire_M']
+   target_resist_M = df.loc[:, 'SuicideResist_M']
+   target_halfhour_M = df.loc[:, 'SuicideLastHalfHour_M']
+   target_composite_M = df.loc[:, ('SuicideComposite_M')]
+   """
+   return df.loc[:, column]
 
-phone_data = df.loc[:, ('Creativity', 'Education', 'Entertainment', 'Information_Reading', 'Productivity_Finance', 'Social',
- 'Travel', 'Utilities', 'Shopping_Food', 'Other', 'Games', 'Health and Fitness')]
+def get_morning(df):
+   morning_survey = df.loc[:, ('TimeToBed', 'ElapsedSleepWait', 'NumWokeUp', 'ElapsedWokeUp', 'NightmareLog', 'NightmareScore',
+   'TimeAwake', 'ElapsedSleepTotal', 'SleepQuality', 'Age', 'Sex', 'IQ', 'Race')]
 
-le = preprocessing.LabelEncoder()
-#each column may need its own label encoder?
-#consider looping through df and making an encoder for each one and storing in a dict
+   return morning_survey
+
+def get_phone(df):
+   phone_data = df.loc[:, ('Creativity', 'Education', 'Entertainment', 'Information_Reading', 'Productivity_Finance', 'Social',
+   'Travel', 'Utilities', 'Shopping_Food', 'Other', 'Games', 'Health and Fitness')]
